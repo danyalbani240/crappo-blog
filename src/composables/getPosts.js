feed994a0a59525248f49548224413f7d3bfdb55
DanyalBani <mr.d.bani.d@gmail.com>
2021-10-28 09:30:05 +0330

    adding realtime listener

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,12 +1,20 @@
 import { projectFirestore } from "../firebase/config";
 const loadPosts = async (theNode) => {
   try {
-    const res = await projectFirestore.collection("articles")
-    .orderBy('createdAt','desc')
-    .get();
-    theNode.value = res.docs.map((doc) => {
-      return { ...doc.data(), id: doc.id };
-    });
+    const res = await projectFirestore
+      .collection("articles")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snap) => {
+        let docs = snap.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        });
+        console.log(docs);
+        theNode.value = docs;
+      });
+    // .get();
+    // theNode.value = res.docs.map((doc) => {
+    //   return { ...doc.data(), id: doc.id };
+    // });
   } catch (err) {
     //error.value = err.message;
     console.log(err);
