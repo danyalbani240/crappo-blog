c3c2ced3dc10e994b6df2c7eda8c603fe66f89cb
DanyalBani <mr.d.bani.d@gmail.com>
2021-10-28 09:06:19 +0330

    order Posts By the Created Time

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,7 +1,9 @@
 import { projectFirestore } from "../firebase/config";
 const loadPosts = async (theNode) => {
   try {
-    const res = await projectFirestore.collection("articles").get();
+    const res = await projectFirestore.collection("articles")
+    .orderBy('createdAt','desc')
+    .get();
     theNode.value = res.docs.map((doc) => {
       return { ...doc.data(), id: doc.id };
     });
